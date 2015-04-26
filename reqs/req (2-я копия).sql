a0e766e85734b34343a200dbb6ac8d927f10dfdc
clock <clock@isc.tsu.ru>
2015-04-26 14:18:19 +0600

    1st request

---
+++ reqs/req (2-я копия).sql
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+
+select	company.name, branch_office.city_name,
+		department.name, service.name, service.cost,
+		service.requirements
+from company, branch_office, office, department, service
+where	branch_office.idCompany = company.name and
+		office.idBranch = branch_office.id and
+		department.idOffice = office.address and
+		service.idDepart = department.name
+group by company.name;

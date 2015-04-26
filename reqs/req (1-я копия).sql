3c2590af1e61ac2759bac98ee6f328ce23fcdda4
clock <clock@isc.tsu.ru>
2015-04-26 13:47:15 +0600

    added 1st request

---
+++ reqs/req (1-я копия).sql
@@ -0,0 +1,8 @@
+select *
+from employee
+where	employee.name not like 'Т%' and
+		(year(date())-year(employee.bday)) as age > 25 and
+		-- month(employee.vacancy) as vac_month < 6 and
+		-- vac_month > 8
+		employee.salary < 15000
+group by employee.name;

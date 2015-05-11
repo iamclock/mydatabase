96fd70eb55005e88a4ab2f30b1b1c9536c69c2d2
clock <clock@isc.tsu.ru>
2015-05-11 23:58:29 +0600

    1st request modified + added delete tables script

---
+++ reqs/del.sql
@@ -0,0 +1,12 @@
+drop table claim;
+
+drop table employee;
+drop table ordered_service;
+drop table service;
+drop table _order;
+drop table department;
+drop table client;
+drop table office;
+drop table branch_office;
+drop table company;
+show tables;
--- reqs/req1.sql
+++ reqs/req1.sql
@@ -2,6 +2,6 @@ select employee.*
 from employee
 where	employee.name not like 'Т%' and
 		date_add(employee.bday, interval 25 year) >= curdate() and
-		vacancy = 1 and
+		employee.vacancy = true and
 		employee.salary < 15000
 order by employee.name;

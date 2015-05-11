95cd7445092201e3c821ea030377d5c81ab66636
clock <clock@isc.tsu.ru>
2015-05-12 01:36:19 +0600

    3rd and 6th requests modified

--- reqs/req3.sql
+++ reqs/req3.sql
@@ -1,4 +1,4 @@
-select office.address, count(employee.id) as count
+select office.address, count(employee.id) as number
 from employee join department
 on employee.idDepart = department.name, office
 where office.address = department.idOffice
--- reqs/req6.sql
+++ reqs/req6.sql
@@ -1,2 +1,4 @@
-select ordered_service.id
-from ordered_service where max(count(claim.id))
+select ordered_service.idOrder
+from ordered_service join claim on
+	ordered_service.idOrder = claim.idOrd_ordered_serv
+order by count(claim.id) desc limit 1

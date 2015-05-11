dd9f057c69dd786087fb1afd5d4d23a81463ec58
clock <clock@isc.tsu.ru>
2015-05-11 23:57:00 +0600

    mysql.sh added some strings

--- mysql.sh
+++ mysql.sh
@@ -3,5 +3,6 @@
 
 
 
-mysql -h 192.168.0.116 -u 1125_st -D 1125_st --default-character-set=utf8 -p
+#mysql -h 192.168.0.116 -u 1125_st -D 1125_st --default-character-set=utf8 -p
 # echo QFqSW3Ko
+mysql -u porsche --default-character-set=utf8 --password=''

1d853004d94169ff864b3f2003f488c291e019ef
clock <clock@isc.tsu.ru>
2015-05-12 02:32:08 +0600

    10th request modified goal in commentary inside file

--- reqs/req10.sql
+++ reqs/req10.sql
@@ -1,3 +1,7 @@
-select office.address, department.name, max(count(service.id))
-from office where department.idOffice = office.address and
-department.name = service.idDepart
+select	obj1.address, obj2.name as department,
+		count(obj3.id) as number
+from office as obj1 join department obj2 join service as obj3
+on obj1.address = obj2.idOffice and obj2.name = obj3.idDepart
+order by obj1.address
+
+#найти максимальный count

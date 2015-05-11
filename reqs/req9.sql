324acea8ef2f6342c607263a0c8c64287678021e
clock <clock@isc.tsu.ru>
2015-05-12 02:31:10 +0600

    9th request modified goal in commentary inside

--- reqs/req9.sql
+++ reqs/req9.sql
@@ -2,3 +2,5 @@ select employee.name, employee.post, employee.bday, department.name
 from employee where employee.idDepart = department.name and
 distinct employee.bday
 order by employee.name
+
+#найти у кого совпадает и проигнорировать их

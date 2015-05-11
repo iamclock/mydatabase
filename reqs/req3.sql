1ff0a93c5f3acb47baf5f636172c4f696214115d
clock <clock@isc.tsu.ru>
2015-05-12 01:10:44 +0600

    3rd request modified

--- reqs/req3.sql
+++ reqs/req3.sql
@@ -1,4 +1,4 @@
-select office.address, count(employee.id)
+select office.address, count(employee.id) as count
 from employee join department
 on employee.idDepart = department.name, office
 where office.address = department.idOffice

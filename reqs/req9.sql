547062a206d76d789be90effd34779a50ce30bfe
clock <clock@isc.tsu.ru>
2015-05-13 23:04:53 +0600

    9th request done

--- reqs/req9.sql
+++ reqs/req9.sql
@@ -1,6 +1,6 @@
-select employee.name, employee.post, employee.bday, department.name
-from employee where employee.idDepart = department.name and
-distinct employee.bday
-order by employee.name
+select department.name as department, e1.name, e1.post, e1.bday
+from department, employee as e1 join employee as e2
+on month(e1.bday) = month(e2.bday)
+where e2.id is null and department.name = e1.idDepart
+order by department.name
 
-#найти у кого совпадает и проигнорировать их

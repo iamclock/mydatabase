5df8a537ea9883611f39a90521f7dcf682be8b1f
clock <double7@mail.com>
2015-05-14 12:19:22 +0600

    req1.sql modified

--- reqs/req1.sql
+++ reqs/req1.sql
@@ -1,7 +1,7 @@
 select employee.*
 from employee
 where	employee.name not like 'Т%' and
-		date_add(employee.bday, interval 25 year) >= curdate() and
+		date_add(employee.bday, interval 25 year) < curdate() and
 		employee.vacancy = true and
 		employee.salary < 15000
 order by employee.name;

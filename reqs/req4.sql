e6b89c637f3f8a7c5627aa4a956363b54218d300
clock <double7@mail.com>
2015-05-14 12:20:34 +0600

    req4.sql modified

--- reqs/req4.sql
+++ reqs/req4.sql
@@ -1,2 +1,16 @@
+#select client.bday, _order.submission_date, employee.bday
+#from client, _order, employee
+
+
+
+
+
 select client.bday, _order.submission_date, employee.bday
-from client, _order, employee
+#union all
+
+
+
+
+
+
+#сделать одну таблицу с перечнем всех дат объединением

11d571279772ba0e35dbe045dd79195807a44877
clock <double7@mail.com>
2015-05-14 12:20:10 +0600

    req3.sql modified

--- reqs/req3.sql
+++ reqs/req3.sql
@@ -2,4 +2,4 @@ select office.address, count(employee.id) as number
 from employee join department
 on employee.idDepart = department.name, office
 where office.address = department.idOffice
-order by office.address desc;
+group by office.address;

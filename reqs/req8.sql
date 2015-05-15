3a9fd6252d2e05fc53d2af9ae6c40e70fcc06cf5
clock <double7@mail.com>
2015-05-15 19:56:31 +0600

    req8.sql modified

--- reqs/req8.sql
+++ reqs/req8.sql
@@ -1,18 +1,15 @@
 select summary.idOrder, summary.summ as entire
-from
-			(select sum(t3.cost) as summ, t2.idOrder
+	from
+		(select sum(t3.cost) as summ, t2.idOrder
 			from _order as t1 join ordered_service as t2 join service as t3
-			on t1.id = t2.idOrder and t2.idService = t3.id
+				on t1.id = t2.idOrder and t2.idService = t3.id
 			group by t2.idOrder
-			) as summary
-where	summary.summ < #any (select max(summary.summ)
+		) as summary
+	where	summary.summ < #any (select max(summary.summ)
 			#from summary
 			) and
-		summary.summ > #any (select min(summary.summ)
-			#from summary или продублировать таблицу сверху
+		summary.summ > #any (select min(summary.summ) или продублировать таблицу сверху
 			) and
 		#summary.idOrder = ordered_service.idOrder
-order by summary.idOrder
+	order by summary.idOrder
 
-
-# Invalid use of group function в строках 6 и 7

bfeaa27a199d3569ac664d15bb0798ecb237486d
clock <double7@mail.com>
2015-05-15 19:56:23 +0600

    req7.sql modified

--- reqs/req7.sql
+++ reqs/req7.sql
@@ -1,10 +1,9 @@
 select
 	department.name, employee.name, employee.bday,
 	employee.post, employee.salary
-from (select
-
-
-
-)
-department join employee on department.name = employee.idDepart
-order by rand() limit 2
+	from (select name
+					from department
+					order by rand() limit 2
+				)
+	department join employee on department.name = employee.idDepart
+	order by department.name

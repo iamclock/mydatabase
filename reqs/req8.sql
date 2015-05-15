7ba161d6df51d60857d074553069fc3811171756
clock <clock@isc.tsu.ru>
2015-05-15 23:59:43 +0600

    req8.sql modified

--- reqs/req8.sql
+++ reqs/req8.sql
@@ -5,11 +5,9 @@ select summary.idOrder, summary.summ as entire
 				on t1.id = t2.idOrder and t2.idService = t3.id
 			group by t2.idOrder
 		) as summary
-	where	summary.summ < #any (select max(summary.summ)
-			#from summary
-			) and
-		summary.summ > #any (select min(summary.summ) или продублировать таблицу сверху
-			) and
+	where summary.summ < any(select summary.summ from summary) and
+		summary.summ > any(select summary.summ from summary) #или продублировать таблицу сверху
+		# and
 		#summary.idOrder = ordered_service.idOrder
 	order by summary.idOrder
 

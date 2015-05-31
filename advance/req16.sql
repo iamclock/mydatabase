ab5eace2053edb4e3107483fbb4660ac985b1032
clock <clock@isc.tsu.ru>
2015-05-31 20:40:46 +0600

    req16.sql modified

--- advance/req16.sql
+++ advance/req16.sql
@@ -1,50 +1,42 @@
-/*
-16. Вывести компании у которых обслуживались все клиенты,
-которые есть в базе. При этом эти компании должны
-оказывать ВСЕ услуги, которые есть в базе.
-Вывод: Название компаний.
-*/
 
+# Done
 
-
-
-
-
-
-
-
-
-
-
-
-
-(select count(*) as numb_clients
-	from client
-)
-
-(select count(*) as numb_services
-	from service
-)
-
-select count(*) as clients
-	from
-		(select _order.id as order_id, distinct client.id as client_id
-			from _order join client on _order.idClient = client.id
-		)
-
-
+# схема строения базы данных
+# (idService) ordered_service (idOrder) -> (id) _order (idClient) -> (id) client
+#  |
+# \ /
+#  V
+# (id) service (idDepart) -> (name) department (idOffice) -> (address) office (idBranch) -> (id) branch_office (idCompany) -> (name) company
 
 
 
 select company.name
-	from company join branch_office on company.name = branch_office.idCompany
-		join office on branch_office.id = office.idBranch
-		join department on office.address = department.idOffice
-		join service on department.name = service.idDepart
-		
-		
-		
-select 
-	from _order join ordered_service on _order. = ordered_service.
-		join service on ordered_service. = service
-	
+	from company
+		join #подзапрос будет возвращать название компании и число не повторяющихся клиентов в базе данных
+		(select company.name as comp_name, count(distinct client.id) as number_of_clients
+			from company join branch_office on company.name = branch_office.idCompany
+				join office on branch_office.id = office.idBranch
+				join department on office.address = department.idOffice
+				join service on department.name = service.idDepart
+				join ordered_service on service.id = ordered_service.idService
+				join _order on ordered_service.idOrder = _order.id
+				join client on _order.idClient = client.id
+		) as comp_clients on company.name = comp_clients.comp_name
+		join #подзапрос будет возвращать название компании и число оказываемых не повторяющихся услуг
+		(select company.name as comp_name, count(distinct service.id) as number_of_services
+			from company join branch_office on company.name = branch_office.idCompany
+				join office on branch_office.id = office.idBranch
+				join department on office.address = department.idOffice
+				join service on department.name = service.idDepart
+		) as comp_services on company.name = comp_services.comp_name
+	where comp_clients.number_of_clients =
+		#общее количество клиентов в базе данных
+		(select count(distinct client.id) as entire_numb_of_clients
+			from client
+		)
+	and comp_services.number_of_services =
+		#общее количество услуг в базе данных
+		(select count(distinct service.id) entire_numb_of_services
+			from service
+		)
+

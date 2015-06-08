03b655e67b11f03fe5cef0925e63824f6cb5ce79
clock <double7@mail.com>
2015-06-08 16:13:47 +0600

    req13 modified

--- advance/req13.sql
+++ advance/req13.sql
@@ -1,5 +1,4 @@
 
-
 #Done
 
 # схема строения базы данных
@@ -15,32 +14,17 @@
 
 select _order.id, _order.submission_date,
 if(count(*)*100/entire_numb_of_orders > 74,"Последние заказы",if(count(*)*100/entire_numb_of_orders > 24,"Основные заказы","Первые заказы")) as commentary
-	from company join branch_office on company.name = branch_office.idCompany
-		join office on branch_office.id = office.idBranch
-		join department on office.address = department.idOffice
-		join service on department.name = service.idDepart
-		join ordered_service on service.id = ordered_service.idService
-		join _order on ordered_service.idOrder = _order.id
-		join #заказы компании и их дата
-			(select company.name as comp_name, _order.submission_date as date
-				from company join branch_office on company.name = branch_office.idCompany
-					join office on branch_office.id = office.idBranch
-					join department on office.address = department.idOffice
-					join service on department.name = service.idDepart
-					join ordered_service on service.id = ordered_service.idService
-					join _order on ordered_service.idOrder = _order.id
-			) as submitted_dates_tbl on company.name = submitted_dates_tbl.comp_name
-			and _order.submission_date > submitted_dates_tbl.date
-		join #общее количество заказов у компании
-			(select company.name as comp_name, count(distinct _order.id) as entire_numb_of_orders
-				from company join branch_office on company.name = branch_office.idCompany
-					join office on branch_office.id = office.idBranch
-					join department on office.address = department.idOffice
-					join service on department.name = service.idDepart
-					join ordered_service on service.id = ordered_service.idService
-					join _order on ordered_service.idOrder = _order.id
-				group by company.name
-			) as numb_of_company_orders_tbl on company.name = numb_of_company_orders_tbl.comp_name
+	from _order
+		join # создаётся таблица соответствия даты одного заказа к количеству дат других заказов заказанных раньше
+			(select ord1.id as ord_id, count(*) as grater_sub_dates
+				from _order as ord1
+					join _order as ord2 on ord1.id != ord2.id and ord1.submission_date > ord2.submission_date
+				group by ord1.id
+			) as numbs_greater_sub_dates_tbl on _order.id = numbs_greater_sub_dates_tbl.ord_id
+		join #общее количество заказов
+			(select count(distinct _order.id) as entire_numb_of_orders
+				from _order
+			) as numb_of_orders_tbl
 	order by _order.id
 
 
